9707ddaed30727f4dcfc122e9b4c7a307e9a3538
Alexa Climaco <c1482-23@itmlyceumalabang.onmicrosoft.com>
2025-10-12 10:40:12 +0800

    feat: US001 - Add User entity with auth fields

---
+++ StudentDiary.Infrastructure/Models/User.cs
@@ -0,0 +1,38 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace StudentDiary.Infrastructure.Models
+{
+    public class User
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Username { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(255)]
+        [EmailAddress]
+        public string Email { get; set; } = string.Empty;
+
+        [Required]
+        [StringLength(255)]
+        public string PasswordHash { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string FirstName { get; set; } = string.Empty;
+
+        [StringLength(100)]
+        public string LastName { get; set; } = string.Empty;
+
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+
+        public DateTime? LastLoginAt { get; set; }
+
+        public bool IsActive { get; set; } = true;
+
+        // Navigation property
+        public virtual ICollection<DiaryEntry> DiaryEntries { get; set; } = new List<DiaryEntry>();
+    }
+}
